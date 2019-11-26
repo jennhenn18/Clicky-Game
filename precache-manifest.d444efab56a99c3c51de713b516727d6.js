c34ca6e8a999ecfcb38ab2690460a12d8d4aecde
Jenn Henderson <jenn.henderson18@gmail.com>
2019-11-25 17:47:37 -0800

    Updates

---
+++ precache-manifest.d444efab56a99c3c51de713b516727d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "916726bf970ecf27403c48290a791c9a",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "c42004d3b170dbc62a67",
+    "url": "/Clicky-Game/static/css/main.f0b30a0f.chunk.css"
+  },
+  {
+    "revision": "af2ed4bc2ce8e1526e55",
+    "url": "/Clicky-Game/static/js/2.827cca97.chunk.js"
+  },
+  {
+    "revision": "c42004d3b170dbc62a67",
+    "url": "/Clicky-Game/static/js/main.95b13413.chunk.js"
+  },
+  {
+    "revision": "d860454f3c985e4595f4",
+    "url": "/Clicky-Game/static/js/runtime-main.a399bcbf.js"
+  }
+]);
